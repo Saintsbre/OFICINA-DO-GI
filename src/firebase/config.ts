caf41d27b21a11c1c6ac407ce26856dd48b3c832
Saints <Santosofc2017@gmail.com>
2026-02-23 18:49:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6807116719-83c67",
+  "appId": "1:1623071303:web:4f9a8fe420462594d86f62",
+  "apiKey": "AIzaSyDC1c0KuOzpeT6p1Ic26qPsOJLyGQlYkwU",
+  "authDomain": "studio-6807116719-83c67.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1623071303"
+};
